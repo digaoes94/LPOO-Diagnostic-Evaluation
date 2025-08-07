81b964d2cceb12d04bc373d82e46ec8f661f20c5
Rodrigo Araujo <rasch.dev@gmail.com>
2025-08-07 11:55:35 -0300

    FULL REFACTORING

--- src/application/AppClinica.java
+++ src/application/AppClinica.java
@@ -2,7 +2,7 @@ package application;
 
 import java.util.Scanner;
 
-public class Main {
+public class AppClinica {
 	static Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
